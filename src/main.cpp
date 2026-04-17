bb01bbb5bcb50a8dd26afed1dd17ddf52c857c33
Renan Mendes <renanmzmendes@gmail.com>
2026-04-17 14:04:16 -0300

    feat: read DS18B20 temperature via OneWire

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,38 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "pins.h"
+
+OneWire oneWire(ONE_WIRE_PIN);
+DallasTemperature sensors(&oneWire);
+
+float readTemperatureCelsius() {
+  sensors.requestTemperatures();
+  float temp = sensors.getTempCByIndex(0);
+  if (temp == DEVICE_DISCONNECTED_C) {
+    Serial.println("Sensor read error: DS18B20 disconnected or CRC failure");
+    return NAN;
+  }
+  return temp;
+}
 
 void setup() {
   Serial.begin(115200);
   Serial.println("FishHub firmware booting...");
+
+  sensors.begin();
+  int deviceCount = sensors.getDeviceCount();
+  if (deviceCount == 0) {
+    Serial.println("No DS18B20 found on OneWire bus");
+  } else {
+    Serial.printf("%d DS18B20 sensor(s) found\n", deviceCount);
+  }
 }
 
 void loop() {
-  Serial.println("alive");
+  float temp = readTemperatureCelsius();
+  if (!isnan(temp)) {
+    Serial.printf("Temp: %.2f °C\n", temp);
+  }
   delay(5000);
 }
